6e19d722e4a9dc46ce4320a59c0aade4af0210ff
Alexey Russkov <olmeronline@mail.ru>
2025-02-17 14:59:55 +0400

    Создал homework2.js, в котором написал 10 заданий по второму ДЗ

---
+++ site2/homework1/js/homework2.js
@@ -0,0 +1,45 @@
+let a = 10;
+alert(`Переменная a равняется ${a}`);
+a = 20;
+alert(`А теперь переменная a равняется ${a}`);
+
+let yearFirstIPhone = 2007;
+alert(`Первый iPhone выпустили ${yearFirstIPhone} году`);
+
+let nameCreatorJS = "Брендан Эйх";
+alert(`JavaScript создал ${nameCreatorJS}`)
+
+let number1 = 10;
+let number2 = 2;
+alert(`Сумма 2 чисел: ${number1 + number2}`);
+alert(`Разница 2 чисел: ${number1 - number2}`);
+alert(`Произведение 2 чисел: ${number1 * number2}`);
+alert(`Частное двух чисел: ${number1 / number2}`);
+
+let result = 2 ** 5;
+alert(`2 в 5-ой степени равно ${result}`);
+
+a = 9;
+let b = 2;
+alert(`Остаток от деления 9 на 2 равен ${a % b}`);
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+let age = Number(prompt("Сколько вам лет?"));
+alert(`Вам ${age} лет`);
+
+let user = {
+    name: "Alexey",
+    age: 30,
+    isAdmin: true,
+};
+
+userName = prompt("Как вас зовут?");
+alert(`Привет, ${userName}!`);
